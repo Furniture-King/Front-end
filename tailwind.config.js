62058e911182d422a398401f9e6ca1b541e3cd55
samuel <timeuss.sam@gmail.com>
2021-11-15 16:43:01 +0100

    miscs: add first navBar & config tailwind extends colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-font-black': '#232426',
+        'color-font-gray': '#BBBFC8',
+        'color-font-white': '#F7F7F7',
+        'color-bg-darkBlue': '#151F33',
+        'color-bg-darkBrown': '#2A2419',
+        'color-bg-lightBrown': '#847A68',
+        'color-bg-light': '#F9F5EC',
+        'color-bg-ultraLight': '#FCFAF7'
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
